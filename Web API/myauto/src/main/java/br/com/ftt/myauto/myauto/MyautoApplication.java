d25738158d5ea708118bd3536d1389403e6940d1
Julio Lima <juliow300@gmail.com>
2021-11-13 18:05:42 -0300

    First commit

---
+++ Web API/myauto/src/main/java/br/com/ftt/myauto/myauto/MyautoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ftt.myauto.myauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyautoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyautoApplication.class, args);
+	}
+
+}
---
+++ Web API/myauto/src/test/java/br/com/ftt/myauto/myauto/MyautoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ftt.myauto.myauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyautoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
